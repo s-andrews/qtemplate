2fda88265f7b924af13b933aa6fdcdbc851820db
s-andrews <simon@proteo.me.uk>
2026-02-24 10:13:07 +0000

    Added more docs, checks and updated README

--- R/new_doc.R
+++ R/new_doc.R
@@ -4,6 +4,7 @@
 #' @param template The name of the template (defaults to standard Babraham template)
 #' @param author Your name for the report
 #' @param title The title of the document
+#' @param overwrite A logical to say if we should force overwriting if the outfile exists
 #' @param theme Which ggplot theme to install (default theme_bw)
 #'
 #' @returns Nothing, but a new document is created for you
@@ -11,8 +12,41 @@
 #'
 #' @examples
 #' new_doc("test.qmd", author="Jane Doe", title="My new analysis", theme="theme_dark")
-new_doc <- function(outfile,template="babraham_bioinformatics.qmd", author="Simon Andrews", title="Enter Title Here", theme="theme_bw") {
+new_doc <- function(
+    outfile,
+    template="babraham_bioinformatics.qmd",
+    author="Simon Andrews",
+    title="Enter Title Here",
+    theme="theme_bw",
+    overwrite=FALSE) {
   template_file <- system.file("extdata",template,package="qtemplate")
+
+  if (!endsWith(tolower(template),".qmd")) {
+    stop("Template ",template," doesn't end with .qmd - can't be right")
+  }
+
+  if (!endsWith(tolower(outfile),".qmd")) {
+    stop("Outfile ",outfile," doesn't end with .qmd - it needs to!")
+  }
+
+
+  if (stringr::str_detect("[/\\:]",template)) {
+    stop(paste0("Invalid chars in template name"))
+  }
+
+  if(!file.exists(template_file)) {
+    stop(paste0("Unknown template ",template))
+  }
+
+  if(file.exists(outfile)) {
+    if(overwrite) {
+      warning(paste0("Outfile ",outfile," exists, but going ahead as overwrite is true"))
+    }
+    else {
+      stop(paste0("Outfile ",outfile," already exists - stopping"))
+    }
+  }
+
   readr::read_file(template_file) -> template_string
 
   list (
--- README.md
+++ README.md
@@ -1,2 +1,34 @@
-# qtemplate
-Quarto Template for starting Babraham Bioinformatics Projects
+# qtemplate - Quickly make new standard Quarto Documents
+
+This R package was designed to be used by the Babraham Bioinformatics group for their analysis work.  It provides a simple way to make a new quarto document based on a template.  Using this is a quick way to start new projects.
+
+
+## Installation
+
+You can install this package directly from github using:
+
+```
+remotes::install_github("s-andrews/qtemplate")
+```
+
+..or..
+
+```
+devtools::install_github("s-andrews/qtemplate")
+```
+
+## Usage
+
+The package only provides a single function ``new_doc``.  This requires the name of the new quarto document you want to create, which must end with a .qmd extension.
+
+You can then optionally provide additional information
+
+* **title**: The title for the document - must end with ```.qmd```
+* **author**: Your name to go in the author field
+* **theme**: The name of a ggplot theme to use, eg "theme_bw", "theme_dark" etc.
+
+By default the new file won't be created if a file with that name exists already. If you *really* want to do that you can set ```overwrite=TRUE```
+
+
+
+
